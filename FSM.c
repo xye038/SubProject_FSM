fb5d5c161937f501c64054205ee6eb471a7ea820
yexin <yexin@jubeiot.com>
2022-10-13 14:56:00 +0800

    使用链表实现状态机

--- FSM.c
+++ FSM.c
@@ -1,136 +1,37 @@
-#include "FSM.h"
+#include "Xin.h"
 
-static FsmTable_T *HeadFsmTable = NULL;
+static struct FSM_s *Head_FSM = NULL;
 
-/*==================================================================
-* Function    : FSM_StateTransfer
-* Description : 状态转换
-* Input Para  : pFsm 状态机对象，
-* Return Value:
-==================================================================*/
-static void FSM_StateTransfer(FSM_T *pFsm, uint8_t state)
+void Fsm_Create(FSM_t *fsm, uint8_t Event,
+                uint8_t Current_System_State,
+                void (*EventActFunc)(void *)
+                )
 {
-    pFsm->curState = state;
-}
-
-/*==================================================================
-* Function    : Add_FsmTable
-* Description : 使用单链表将状态迁移表连接起来
-* Input Para  : 状态迁移表指针
-* Return Value:
-==================================================================*/
-static void Add_FsmTable(FsmTable_T * fsmtable)
-{
-   fsmtable->next = HeadFsmTable;
-   HeadFsmTable = fsmtable;
-}
-
-/*==================================================================
-* Function    : FsmTable_Delete
-* Description : 删除某一个节点
-* Input Para  : 状态迁移表指针
-* Return Value:
-==================================================================*/
-void FsmTable_Delete(FsmTable_T *fsmtable)
-{
-  struct FsmTable_s* curr;
-  if(fsmtable == HeadFsmTable)
-  {
-    HeadFsmTable = fsmtable->next;
-    return;
+   fsm->CurState = Current_System_State;
+   fsm->eventActFun = EventActFunc;       //鍥炶皟鍑芥暟闇�鐢ㄦ埛鑷繁瀹炵幇
+   fsm->event = Event;
+   fsm->Next = Head_FSM;
+   Head_FSM = fsm;
   }
 
-  for(curr = HeadFsmTable; curr!=NULL; curr=curr->next)
+void Fsm_Process(void)
+{
+  struct FSM_s *pass_fsm;
+  for(pass_fsm = Head_FSM; pass_fsm != NULL; pass_fsm = pass_fsm->Next)
   {
-    if(curr->next == fsmtable)
+    if((pass_fsm->CurState==System_State)&&(pass_fsm->event==Trigger_Event))
     {
-      curr->next = fsmtable->next;
-     return;
+      pass_fsm->eventActFun(NULL);
     }
   }
 }
 
-/*==================================================================
-* Function  : FSM_EventHandle
-* Description : 状态机处理函数
-* Input Para  : pFsm状态机对象, event触发事件, parm动作执行参数
-* Output Para :
-* Return Value:
-==================================================================*/
-void FSM_EventHandle(FSM_T *pFsm, uint8_t event, void *parm)
+void FSM_INIT(void)
 {
-  struct FsmTable_s* pass_fsm;
-  /*当前状态需要执行的事件函数*/
-  void (*eventActFun)(void *) = NULL;
-  uint8_t NextState;
-  /*获取当前状态号*/
-  uint8_t CurState = pFsm ->curState;
-  uint8_t flag = 0;
-  /*遍历状态链表*/
-  for(pass_fsm = HeadFsmTable; pass_fsm != NULL; pass_fsm = pass_fsm->next)
-  {
-    /*当且仅当当前状态下来个对应的事件才执行*/
-    if(event == pass_fsm->event && CurState == pass_fsm->CurState)
-    {
-      /*允许执行状态事件*/
-      flag = 1;
-      /*获取当前状态事件执行函数*/
-      eventActFun = pass_fsm->eventActFun;
-      /*获取下一个状态事件号*/
-      NextState = pass_fsm->NextState;
-      break;
-    }
-  }
-  /*开始执行状态事件*/
-  if(flag)
-  {
-    /*如果当前事件状态有需要执行的函数，就执行*/
-    if(eventActFun != NULL)
-    {
-        eventActFun(parm);   // 执行相应动作
-    }
-    /*状态转换*/
-    FSM_StateTransfer(pFsm, NextState);
-  }
+  /*
+  Fsm_Create(&Main_CodeQ,Set_LongDown_Event,Main_State,MainState_SetLongDownEvent_Func);
+  Fsm_Create(&MaxShow_CodeQ,Set_LongDown_Event,Max_Range_Show_State,MainState_SetLongDownEvent_Func);
+ */
 }
 
-/*==================================================================
-* Function  : FSM_Regist
-* Description : 状态机注册
-* Input Para  : pFsm状态机对象，pTable状态迁移表，curState当前状态
-* Output Para :
-* Return Value:
-==================================================================*/
-void FSM_Regist(FSM_T *pFsm, FsmTable_T *pTable, uint8_t curState)
-{
-  pFsm->FsmTable = pTable;
-  pFsm->curState = curState;
-}
 
-/*==================================================================
-* Function  : FsmTable_Create
-* Description : 状态迁移表创建
-* Input Para  : 
-* Output Para :
-* Return Value:
-==================================================================*/
-void FsmTable_Create(FsmTable_T *fsmtable, uint8_t event, 
-                    uint8_t curstate, uint8_t nextstate)
-{
-  fsmtable->event = event;
-  fsmtable->CurState = curstate;
-  fsmtable->NextState = nextstate;
-  Add_FsmTable(fsmtable);
-}
-
-/*==================================================================
-* Function  : FsmTable_Attach
-* Description : 将状态迁移表与回调函数映射起来
-* Input Para  : 
-* Output Para :
-* Return Value:
-==================================================================*/
-void FsmTable_Attach(FsmTable_T *fsmtable, Fsm_CallBack fsm_callback)
-{
-  fsmtable->eventActFun = fsm_callback;
-}
